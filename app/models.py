27761e31da1d7b9d3d5903ccbe9b1f502333b341
Sahil Sharma <prototypid@gmail.com>
2023-05-31 20:57:18 +0530

    Add posts table model

---
+++ app/models.py
@@ -0,0 +1,15 @@
+from datetime import timezone
+from sqlite3 import Timestamp
+from .database import Base
+from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.sql.expression import text
+
+
+class Post(Base):
+    __tablename__ = 'posts'
+    id = Column(Integer, primary_key=True, nullable=False)
+    title = Column(String, nullable=False)
+    content = Column(String, nullable=False)
+    published = Column(Boolean, nullable=False, server_default='TRUE')
+    created_at = Column(TIMESTAMP(timezone=True), nullable=True, server_default=text('now()'))
