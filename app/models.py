439ca56399b442c9959c87685d72afb04c17b603
Sahil Sharma <prototypid@gmail.com>
2023-06-01 14:30:04 +0530

    Add user model

--- app/models.py
+++ app/models.py
@@ -1,4 +1,3 @@
-from datetime import timezone
 from .database import Base
 from sqlalchemy import Boolean, Column, Integer, String
 from sqlalchemy.sql.sqltypes import TIMESTAMP
@@ -11,4 +10,13 @@ class Post(Base):
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
     published = Column(Boolean, nullable=False, server_default='TRUE')
-    created_at = Column(TIMESTAMP(timezone=True), nullable=True, server_default=text('now()'))
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
