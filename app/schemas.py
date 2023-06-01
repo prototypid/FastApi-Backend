68f83f3d3309591e744f36664decc0029a909710
Sahil Sharma <prototypid@gmail.com>
2023-06-01 14:31:03 +0530

    Add user schemas

--- app/schemas.py
+++ app/schemas.py
@@ -1,17 +1,33 @@
 from datetime import datetime
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
+
 
 class PostBase(BaseModel):
     title: str
     content: str
     published: bool = True
 
+
 class PostCreate(PostBase):
     pass
+
 
 class PostResponse(PostBase):
     id: int
     created_at: datetime
+
+    class Config:
+        orm_mode = True
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserCreateResponse(BaseModel):
+    id: int
+    email: EmailStr
 
     class Config:
         orm_mode = True
