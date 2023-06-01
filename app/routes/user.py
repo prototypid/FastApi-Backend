be6ebd60f76884b8dda89e705adcb8c0c7e70dbb
Sahil Sharma <prototypid@gmail.com>
2023-06-01 14:38:54 +0530

    Add user routes

---
+++ app/routes/user.py
@@ -0,0 +1,33 @@
+from fastapi import APIRouter, status, HTTPException, Depends
+from app import schemas, models, utils
+from sqlalchemy.orm import Session
+from app.database import get_db
+
+router = APIRouter(
+    prefix='/users',
+    tags=['User']
+)
+
+
+@router.post('/', status_code=status.HTTP_201_CREATED, response_model=schemas.UserCreateResponse)
+def create_new_user(user_data: schemas.UserCreate, db: Session = Depends(get_db)):
+    pass_hashed = utils.hash_pass(user_data.password)
+    user_data.password = pass_hashed
+    new_user = models.User(**user_data.dict())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
+@router.get('/{user_id}',response_model=schemas.UserCreateResponse)
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f'User with id {user_id} does not exists!'
+        )
+
+    return user
---
+++ app/utils.py
@@ -0,0 +1,11 @@
+from passlib.context import CryptContext
+
+pass_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+
+def hash_pass(password):
+    return pass_context.hash(password)
+
+
+def verify_pass(password, hashed_pass):
+    pass_context.verify(password, hashed_pass)
