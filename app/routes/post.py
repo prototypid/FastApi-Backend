fbc5b513709ef0a53d774ac468f35c62e842dd84
Sahil Sharma <prototypid@gmail.com>
2023-06-02 08:07:30 +0530

    Add authentication to routes

--- app/routes/post.py
+++ app/routes/post.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
 from app.database import get_db
 from typing import List
-from app import schemas, models
+from app import schemas, models, oauth2
 
 
 router = APIRouter(
@@ -19,7 +19,12 @@ def get_all_posts(db: Session = Depends(get_db)):
 
 @router.post('/', status_code=status.HTTP_201_CREATED, response_model=schemas.PostResponse)
 # def create_new_post(data: dict = Body()):
-def create_new_post(data: schemas.PostCreate, db: Session = Depends(get_db)):
+def create_new_post(
+        data: schemas.PostCreate,
+        db: Session = Depends(get_db),
+        user=Depends(oauth2.get_current_user)
+):
+    print(user)
     post = models.Post(**data.dict())
     db.add(post)
     db.commit()
@@ -40,20 +45,29 @@ def get_post_with_id(post_id: int, db: Session = Depends(get_db)):
 
 
 @router.delete('/{post_id}', status_code=status.HTTP_204_NO_CONTENT)
-def delete_post_with_post_id(post_id: int, db: Session = Depends(get_db)):
-    post = db.query(models.Post).filter(models.Post.id == post_id)
-    if post.first() is None:
+def delete_post_with_post_id(
+        post_id: int,
+        db: Session = Depends(get_db),
+        user=Depends(oauth2.get_current_user)
+):
+    post_query = db.query(models.Post).filter(models.Post.id == post_id)
+    if post_query.first() is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f'Post wit id {post_id} was not found!'
         )
 
-    post.delete(synchronize_session=False)
+    post_query.delete(synchronize_session=False)
     db.commit()
 
 
 @router.put('/{post_id}', response_model=schemas.PostResponse)
-def update_post_with_id(post_id: int, data: schemas.PostCreate, db: Session = Depends(get_db)):
+def update_post_with_id(
+        post_id: int,
+        data: schemas.PostCreate,
+        db: Session = Depends(get_db),
+        user=Depends(oauth2.get_current_user)
+):
     post_query = db.query(models.Post).filter(models.Post.id == post_id)
     post = post_query.first()
     if not post:
